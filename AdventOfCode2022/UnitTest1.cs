3ad6e0ba60f9afcf89a0dbe6fddc30765c977111
Mateusz <mateuszmaj@o2.pl>
2022-12-01 22:11:37 +0100

    Initial commit

---
+++ AdventOfCode2022/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode2022;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
