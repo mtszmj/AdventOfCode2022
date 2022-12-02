b94a1b6c996a57a0afc47043fe31a5cb839dd048
Mateusz <mateuszmaj@o2.pl>
2022-12-02 22:06:48 +0100

    Day 2 Part 1

---
+++ AdventOfCode2022/Day02.cs
@@ -0,0 +1,66 @@
+namespace AdventOfCode2022;
+
+public static class Day02
+{
+    public static int Calculate(string input)
+    {
+        return input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => (enemy: x[0], me: x[2]))
+            .Select(x => SelectionResult(x) + MatchResult(x))
+            .Sum();
+    }
+
+    public static int SelectionResult((char enemy, char me) rps)
+    {
+        return rps.me switch
+        {
+            'X' => 1,
+            'Y' => 2,
+            'Z' => 3,
+            _ => throw new InvalidOperationException()
+        };
+    }
+    
+    public static int MatchResult((char enemy, char me) rps)
+    {
+        return rps switch
+        {
+            ('A', 'X') => 3,
+            ('A', 'Y') => 6,
+            ('A', 'Z') => 0,
+            ('B', 'X') => 0,
+            ('B', 'Y') => 3,
+            ('B', 'Z') => 6,
+            ('C', 'X') => 6,
+            ('C', 'Y') => 0,
+            ('C', 'Z') => 3,
+            _ => throw new InvalidOperationException()
+        };
+    }
+}
+
+[TestFixture]
+public class Day02Tests
+{
+    private readonly string _example = @"A Y
+B X
+C Z";
+
+    [Test]
+    public void Part1Example()
+    {
+        var result = Day02.Calculate(_example);
+
+        result.Should().Be(15);
+    }
+    
+    [Test]
+    public void Part1Input()
+    {
+        var input = File.ReadAllText("Data/Day02.txt");
+
+        var result = Day02.Calculate(input);
+
+        result.Should().Be(15);
+    }
+}
