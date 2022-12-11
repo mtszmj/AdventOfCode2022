21afd4a789a3c4ba06985d1961201f8d90a3d843
Mateusz <mateuszmaj@o2.pl>
2022-12-11 17:38:03 +0100

    Day 8 Part 2

--- AdventOfCode2022/Day08.cs
+++ AdventOfCode2022/Day08.cs
@@ -84,6 +84,59 @@ public class Day08
         return isVisible.Select(x => x).SelectMany(x => x).Count(x => x);
     }
 
+    public int Part2(string input)
+    {
+        var trees = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Select(c => int.Parse($"{c}")).ToArray()).ToArray();
+
+        var rowsCount = trees.GetLength(0);
+        var colsCount = trees[0].GetLength(0);
+
+        var results = new int[rowsCount, colsCount];
+
+        for (var row = 0; row < rowsCount; row++)
+        {
+            for (var col = 0; col < colsCount; col++)
+            {
+                int left, right, up, down;
+                left = right = up = down = 0;
+
+                // up
+                for (var checkedRow = row - 1; checkedRow >= 0; checkedRow--)
+                { 
+                    up++;
+                    if (trees[checkedRow][col] >= trees[row][col])
+                        break;
+                }
+                // down
+                for (var checkedRow = row + 1; checkedRow < rowsCount; checkedRow++)
+                {
+                    down++;
+                    if (trees[checkedRow][col] >= trees[row][col])
+                        break;
+                }
+                // left
+                for (var checkedCol = col - 1; checkedCol >= 0; checkedCol--)
+                {
+                    left++;
+                    if (trees[row][checkedCol] >= trees[row][col])
+                        break;
+                }
+                // right
+                for (var checkedCol = col + 1; checkedCol < colsCount; checkedCol++)
+                {
+                    right++;
+                    if (trees[row][checkedCol] >= trees[row][col])
+                        break;
+                }
+
+                results[row, col] = up * down * left * right;
+            }
+        }
+
+        return results.Cast<int>().Max();
+    }
+    
     private void Print(int[][] trees, bool[][] isVisible)
     {
         var rowsCount = trees.GetLength(0);
@@ -122,6 +175,18 @@ public class Day08Tests
     public void Part1Input()
     {
         new Day08().Part1(Helper.ReadDay(8)).Should().Be(1698);
+    }
+    
+    [Test]
+    public void Part2Example()
+    {
+        new Day08().Part2(_example).Should().Be(8);
+    }
+    
+    [Test]
+    public void Part2Input()
+    {
+        new Day08().Part2(Helper.ReadDay(8)).Should().Be(672280);
     }
 
 }
