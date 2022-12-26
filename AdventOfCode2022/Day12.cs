543979344ee004548097736e61ddf56259a96b3d
Mateusz <mateuszmaj@o2.pl>
2022-12-26 23:12:58 +0100

    Day 12 Part 2

--- AdventOfCode2022/Day12.cs
+++ AdventOfCode2022/Day12.cs
@@ -4,32 +4,22 @@ public class Day12
 {
     public int Part1(string input)
     {
-        var i = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-        var map = new MapTile[i.Length, i[0].Length];
-        MapTile start = default;
-        MapTile end = default;
-        for (var row = 0; row < i.Length; row++)
-        {
-            var line = i[row];
-            for (var col = 0; col < line.Length; col++)
-            {
-                var c = line[col];
-                if (c == 'S')
-                    map[row, col] = start = new MapTile(row, col, 1, false, false);
-                else if (c == 'E')
-                    map[row, col] = end = new MapTile(row, col, 'z' - 'a' + 1, false, true);
-                else map[row, col] = new MapTile(row, col, c - 'a' + 1, false, false);
-            }
-        }
+        var map = MapTiles(input, out var queue, false);
+        return Solve(queue, map);
+    }
 
-        var queue = new PriorityQueue<MapTile, int>();
-        var current = start;
-        var currentMoves = 0;
+    public int Part2(string input)
+    {
+        var map = MapTiles(input, out var queue, true);
+        return Solve(queue, map);
+    }
+
+    private static int Solve(PriorityQueue<MapTile, int> queue, MapTile[,] map)
+    {
         var iter = 0;
-        queue.Enqueue(current, 0);
         while (true)
         {
-            if (!queue.TryDequeue(out current, out currentMoves))
+            if (!queue.TryDequeue(out var current, out var currentMoves))
                 throw new InvalidOperationException($"No data in queue!? Iterations: {iter}");
 
             // Console.WriteLine($"[{current.Row},{current.Col}]");
@@ -60,12 +50,46 @@ public class Day12
                     continue;
 
                 if (map[move.Item1, move.Item2].Visited) continue;
-                
+
                 if (map[move.Item1, move.Item2].Value > current.Value + 1) continue;
 
                 queue.Enqueue(map[move.Item1, move.Item2], currentMoves + 1);
             }
         }
+    }
+
+    private static MapTile[,] MapTiles(string input, out PriorityQueue<MapTile, int> queue, bool allTilesWithValue1)
+    {
+        var i = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        var map = new MapTile[i.Length, i[0].Length];
+        queue = new PriorityQueue<MapTile, int>();
+        for (var row = 0; row < i.Length; row++)
+        {
+            var line = i[row];
+            for (var col = 0; col < line.Length; col++)
+            {
+                var c = line[col];
+                if (c == 'S')
+                {
+                    map[row, col] =  new MapTile(row, col, 1, false, false);
+                    queue.Enqueue(map[row, col], 0);
+                }
+                else if (c == 'E')
+                {
+                    map[row, col] = new MapTile(row, col, 'z' - 'a' + 1, false, true);
+                }
+                else
+                {
+                    map[row, col] = new MapTile(row, col, c - 'a' + 1, false, false);
+                    if (allTilesWithValue1 && map[row, col].Value == 1)
+                    {
+                        queue.Enqueue(map[row, col], 0);
+                    }
+                }
+            }
+        }
+
+        return map;
     }
 }
 
@@ -106,5 +130,23 @@ abdefghi";
     public void Part1Input()
     {
         new Day12().Part1(Helper.ReadDay(12)).Should().Be(423);
+    }
+    
+    [Test]
+    public void Part2Example()
+    {
+        var input = @"Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi";
+
+        new Day12().Part2(input).Should().Be(29);
+    }
+
+    [Test]
+    public void Part2Input()
+    {
+        new Day12().Part2(Helper.ReadDay(12)).Should().Be(416);
     }
 }
